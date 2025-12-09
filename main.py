a74a60e2e6ec0d9ef5d26e28261355f9708bebcb
Aarav261 <anu0310v@gmail.com>
2025-12-09 23:23:49 +1100

    improved main file for URL selection abstraction'

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 def main():
-    from scraper import extract_meetup_links, extract_meetup_events
+    from Event_scraper import extract_meetup_links, extract_meetup_events
     from database import insert_event
+    from internship_scraper import extract_internship_links
 
-    links = extract_meetup_links()
+    URL_OF_INTERNSHIP_LISTING_PAGE = "https://au.gradconnection.com/internships/engineering-software/melbourne/"
+
+    URL_OF_MEETUP_LISTING_PAGE = "https://www.meetup.com/en-AU/find/?keywords=tech&location=au--Canterbury&source=EVENTS&distance=tenMiles"
+    internship_links = extract_internship_links(URL_OF_INTERNSHIP_LISTING_PAGE)
+    
+    links = extract_meetup_links(URL_OF_MEETUP_LISTING_PAGE)
     events = extract_meetup_events(links)
 
     for event in events:
         insert_event(event)
+        
+        
 
 if __name__ == "__main__":
     
